d0a1eb9fae894824a210253e6a20a9a5ec1eaad0
Junsasaki0077 <ikasas0077@gmail.com>
2021-05-03 11:25:47 +0900

    Install Typescript

--- src/javascripts/reactApp.jsx
+++ src/javascripts/reactApp.jsx
@@ -1,10 +1,13 @@
 import ReactDom from "react-dom";
 import * as React from "react";
 
-const App = (prop) => {
-  return <div style={{ color: "#000" }}>Hello,React App!</div>;
-};
+import Alert from './Alert.tsx';
 
+const App = (prop) => {
+  return <div style={{ color: "#000" }}>Hello,React App!
+  <Alert message="Success!" />
+  </div>;
+};
 const reactRoot = document.getElementById("react-root");
 if (reactRoot) {
   ReactDom.render(<App />, reactRoot);
